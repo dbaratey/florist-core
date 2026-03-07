6e53b4943c30f8cf433ac2038f0e132a987f4bbd
dbaratey <dmitry@baratey.ru>
2026-03-07 19:03:18 +0300

    Create handlers.go

---
+++ internal/production/application/handlers.go
@@ -0,0 +1,38 @@
+package application
+
+import (
+	"context"
+	"github.com/dbaratey/florist-core/internal/production/domain"
+	"github.com/dbaratey/florist-core/internal/shared/kernel"
+)
+
+type RecipeRepository interface {
+	Save(ctx context.Context, recipe *domain.Recipe) error
+}
+
+type CreateRecipeCommand struct {
+	Name        string
+	Ingredients []domain.Ingredient
+}
+
+type CreateRecipeHandler struct {
+	repo RecipeRepository
+}
+
+func NewCreateRecipeHandler(repo RecipeRepository) *CreateRecipeHandler {
+	return &CreateRecipeHandler{repo: repo}
+}
+
+func (h *CreateRecipeHandler) Handle(ctx context.Context, cmd CreateRecipeCommand) (kernel.RecipeID, error) {
+	id := kernel.NewRecipeID()
+	recipe, err := domain.NewRecipe(id, cmd.Name, cmd.Ingredients)
+	if err != nil {
+		return "", err
+	}
+
+	if err := h.repo.Save(ctx, recipe); err != nil {
+		return "", err
+	}
+
+	return id, nil
+}
