078340034507e97869f4a95b66f1467587b5f726
dbaratey <dmitry@baratey.ru>
2026-03-08 09:49:23 +0300

    feat(production/app): add FindByID to RecipeRepository interface

--- internal/production/application/handlers.go
+++ internal/production/application/handlers.go
@@ -8,6 +8,7 @@ import (
 
 type RecipeRepository interface {
 	Save(ctx context.Context, recipe *domain.Recipe) error
+	FindByID(ctx context.Context, id kernel.RecipeID) (*domain.Recipe, error)
 }
 
 type CreateRecipeCommand struct {
@@ -27,11 +28,11 @@ func (h *CreateRecipeHandler) Handle(ctx context.Context, cmd CreateRecipeComman
 	id := kernel.NewRecipeID()
 	recipe, err := domain.NewRecipe(id, cmd.Name, cmd.Ingredients)
 	if err != nil {
-		return "", err
+		return kernel.RecipeID{}, err
 	}
 
 	if err := h.repo.Save(ctx, recipe); err != nil {
-		return "", err
+		return kernel.RecipeID{}, err
 	}
 
 	return id, nil
